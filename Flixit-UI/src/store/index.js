209c028f60fb2b73621c73d75b5114d631afeae7
Manjunath-Hub10 <128030973+Manjunath-Hub10@users.noreply.github.com>
2023-09-14 15:42:48 +0530

    created account in themoviedb website and created API_KEY and stored inside of utils/constants.js and also index.js file created inside store for redux setup(for API Keys)

---
+++ Flixit-UI/src/store/index.js
@@ -0,0 +1,28 @@
+import {
+    configureStore,
+    createAsyncThunk,
+    createSlice,
+  } from "@reduxjs/toolkit";
+
+//   import { API_KEY, TMDB_BASE_URL } from "../utils/constants";
+//   import axios from "axios";
+
+  const initialState = {
+    movies: [],
+    genresLoaded: false,
+    genres: [],
+  };
+
+  const FlixitSlice = createSlice(
+    {
+        name: "Flixit",
+        initialState,
+        extraReducers: (builder) = {},
+    });
+
+    export const store = configureStore({
+        reducer: {
+            flixit: FlixitSlice.reducer,
+        },
+    })
+

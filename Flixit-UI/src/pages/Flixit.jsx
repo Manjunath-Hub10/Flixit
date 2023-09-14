33ca9111e88f9e15350912aba9d24aaa6e9e7df4
Manjunath-Hub10 <128030973+Manjunath-Hub10@users.noreply.github.com>
2023-09-14 13:29:52 +0530

    created Navbar component along with search button & poweroff button

--- Flixit-UI/src/pages/Flixit.jsx
+++ Flixit-UI/src/pages/Flixit.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../components/Navbar';
+import backckgroundImage from "../assets/home.jpg";
+import movieLogo from "../assets/homeTitle.webp";
+
 
 const Flixit = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+   setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
   return (
     <div>
-      Flixit
+      <Navbar isScrolled={isScrolled}/>
     </div>
   )
 }

ae44a20ada78aa800168566f3b83e5479313f72e
Manjunath-Hub10 <128030973+Manjunath-Hub10@users.noreply.github.com>
2023-09-14 15:09:59 +0530

    created player page and also uploaded a video to use play button

--- Flixit-UI/src/pages/Flixit.jsx
+++ Flixit-UI/src/pages/Flixit.jsx
@@ -5,7 +5,7 @@ import movieLogo from "../assets/homeTitle.webp";
 import { FaPlay } from 'react-icons/fa';
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import styled  from 'styled-components';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 // import { useDispatch, useSelector } from "react-redux";
 // import { fetchMovies, getGenres } from '../store';
 // import Slider from '../components/Slider';
@@ -14,6 +14,8 @@ import styled  from 'styled-components';
 const Flixit = () => {
   const [isScrolled, setIsScrolled] = useState(false);
 
+  const navigate = useNavigate();
+  
   window.onscroll = () => {
    setIsScrolled(window.pageYOffset === 0 ? false : true);
     return () => (window.onscroll = null);
